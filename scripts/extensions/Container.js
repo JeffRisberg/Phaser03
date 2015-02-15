4e65ef1878f18fe32df560313bd1d08e5e76fa9b
Jeff Risberg <jrisberg@aol.com>
2015-02-15 11:56:15 -0800

    created layout function

--- scripts/extensions/Container.js
+++ scripts/extensions/Container.js
@@ -8,6 +8,16 @@ define(['Phaser'], function (Phaser) {
     Container.prototype = Object.create(Phaser.Group.prototype);
     Container.constructor = Container;
 
+    Container.layout = function () {
+        var y = 0;
+        // this is vertical
+        for (child in this.children) {
+            child.x = 0;
+            child.y = y;
+            y += child.height;
+        }
+    };
+
     return Container;
 });
 
